ec1c183b512598d2c786a8dd0651ec1afb29b564
shiiwvv <shivkumar29xyz@gmail.com>
2026-06-20 00:03:57 +0530

    Changes so that website can be published

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter , Outlet , createRoutesFromElements , RouterProvider , Route } from 'react-router-dom'
+import { createHashRouter , Outlet , createRoutesFromElements , RouterProvider , Route } from 'react-router-dom'
 import {Home , NavBar , Pattern , Footer} from './components'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path:'/',
     element:<App/>,
